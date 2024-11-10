6889110005d1d2d356b58d6a27bd15f6a8ca0c35
Barryalitiko <tiobarryx@gmail.com>
2024-11-10 11:47:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,18 @@
-const { makeWASocket, useMultiFileAuthState, DisconnectReason } = require('@whiskeysockets/baileys');
-const path = require('path');
-const qrcode = require('qrcode-terminal');  
+const { connect } = require("./src/connection");
+const { load } = require("./src/loader");
+const { infoLog, bannerLog } = require("./src/utils/logger");
 
-const sessionPath = path.join(__dirname, 'session');
-const { state, saveState } = useMultiFileAuthState(sessionPath);
+async function start() {
+  try {
+    bannerLog();
+    infoLog("Krampus se está iniciando");
 
-const connectKrampus = async () => {
-    const sock = makeWASocket({
-        auth: state, 
-    });
+    const socket = await connect();
 
-    sock.ev.on('qr', (qr) => {
-        console.log('Generando QR...');
-        qrcode.generate(qr, { small: true }); 
-    });
+    load(socket);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-    sock.ev.on('open', () => {
-        console.log('Conexión establecida exitosamente!');
-    });
-
-    sock.ev.on('close', ({ reason }) => {
-        console.log('Conexión cerrada:', reason);
-    });
-
-    sock.ev.on('error', (error) => {
-        console.error('Error:', error);
-    });
-
-    sock.ev.on('auth-state.update', (authState) => {
-        saveState(authState); 
-    });
-
-    return sock;
-};
-
-connectKrampus().catch(console.error);
+start();
