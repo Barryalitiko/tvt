6184cb4d263e8ff8e316846f51f3065ab901aff1
Barryalitiko <tiobarryx@gmail.com>
2024-11-10 18:15:56 +0100

    Update logger.js

--- SRC/utils/logger.js
+++ SRC/utils/logger.js
@@ -1,32 +1,35 @@
 const { version } = require("../../package.json");
 
 exports.sayLog = (message) => {
-  console.log("\x1b[36m[TAKESHI BOT | TALK]\x1b[0m", message);
+  console.log("\x1b[36m[KRAMPUS | TALK]\x1b[0m", message);
 };
 
 exports.inputLog = (message) => {
-  console.log("\x1b[30m[TAKESHI BOT | INPUT]\x1b[0m", message);
+  console.log("\x1b[30m[KRAMPUS | INPUT]\x1b[0m", message);
 };
 
 exports.infoLog = (message) => {
-  console.log("\x1b[34m[TAKESHI BOT | INFO]\x1b[0m", message);
+  console.log("\x1b[34m[KRAMPUS | INFO]\x1b[0m", message);
 };
 
 exports.successLog = (message) => {
-  console.log("\x1b[32m[TAKESHI BOT | SUCCESS]\x1b[0m", message);
+  console.log("\x1b[32m[KRAMPUS | SUCCESS]\x1b[0m", message);
 };
 
 exports.errorLog = (message) => {
-  console.log("\x1b[31m[TAKESHI BOT | ERROR]\x1b[0m", message);
+  console.log("\x1b[31m[KRAMPUS | ERROR]\x1b[0m", message);
 };
 
 exports.warningLog = (message) => {
-  console.log("\x1b[33m[TAKESHI BOT | WARNING]\x1b[0m", message);
+  console.log("\x1b[33m[KRAMPUS | WARNING]\x1b[0m", message);
 };
 
 exports.bannerLog = () => {
-  console.log(`\x1b[36m░▀█▀░█▀█░█░█░█▀▀░█▀▀░█░█░▀█▀░░█▀▄░█▀█░▀█▀\x1b[0m`);
-  console.log(`░░█░░█▀█░█▀▄░█▀▀░▀▀█░█▀█░░█░░░█▀▄░█░█░░█░`);
-  console.log(`\x1b[36m░░▀░░▀░▀░▀░▀░▀▀▀░▀▀▀░▀░▀░▀▀▀░░▀▀░░▀▀▀░░▀░\x1b[0m`);
-  console.log(`\x1b[36m🤖 Versão: \x1b[0m${version}\n`);
+  console.log(`\x1b[34m▄▄   ▄▄▄\x1b[0m`);
+  console.log(`\x1b[34m ██  ██▀\x1b[0m`);
+  console.log(`\x1b[34m ██▄██      ██▄████   ▄█████▄  ████▄██▄\x1b[0m`);
+  console.log(`\x1b[34m █████      ██▀       ▀ ▄▄▄██  ██ ██ ██\x1b[0m`);
+  console.log(`\x1b[34m ██  ██▄    ██       ▄██▀▀▀██  ██ ██ ██\x1b[0m`);
+  console.log(`\x1b[34m ██   ██▄   ██       ██▄▄▄███  ██ ██ ██\x1b[0m`);
+  console.log(`\x1b[34m ▀▀    ▀▀   ▀▀        ▀▀▀▀ ▀▀  ▀▀ ▀▀ ▀▀\x1b[0m`);
 };
