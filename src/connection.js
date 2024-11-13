3374f633f55f85e1f05904c73b49fe13d83b057f
Barryalitiko <tiobarryx@gmail.com>
2024-11-13 01:06:04 +0100

    Create connection.js

---
+++ src/connection.js
@@ -0,0 +1,135 @@
+const path = require("path");
+const { question, onlyNumbers } = require("./utils");
+const {
+  default: makeWASocket,
+  DisconnectReason,
+  useMultiFileAuthState,
+  fetchLatestBaileysVersion,
+  isJidBroadcast,
+  isJidStatusBroadcast,
+  proto,
+  makeInMemoryStore,
+  isJidNewsletter,
+} = require("baileys");
+const NodeCache = require("node-cache");
+const pino = require("pino");
+const { load } = require("./loader");
+const {
+  warningLog,
+  infoLog,
+  errorLog,
+  sayLog,
+  successLog,
+} = require("./utils/logger");
+
+const msgRetryCounterCache = new NodeCache();
+
+const store = makeInMemoryStore({
+  logger: pino().child({ level: "silent", stream: "store" }),
+});
+
+async function getMessage(key) {
+  if (!store) {
+    return proto.Message.fromObject({});
+  }
+
+  const msg = await store.loadMessage(key.remoteJid, key.id);
+
+  return msg ? msg.message : undefined;
+}
+
+async function connect() {
+  const { state, saveCreds } = await useMultiFileAuthState(
+    path.resolve(__dirname, "..", "assets", "auth", "baileys")
+  );
+
+  const { version } = await fetchLatestBaileysVersion();
+
+  const socket = makeWASocket({
+    version,
+    logger: pino({ level: "error" }),
+    printQRInTerminal: false,
+    defaultQueryTimeoutMs: 60 * 1000,
+    auth: state,
+    shouldIgnoreJid: (jid) =>
+      isJidBroadcast(jid) || isJidStatusBroadcast(jid) || isJidNewsletter(jid),
+    keepAliveIntervalMs: 60 * 1000,
+    markOnlineOnConnect: true,
+    msgRetryCounterCache,
+    shouldSyncHistoryMessage: () => false,
+    getMessage,
+  });
+
+  if (!socket.authState.creds.registered) {
+    warningLog("Credencial aún sin configurar...");
+
+    infoLog('Ingrese su número de telefono sin el + (ejemplo: "27734375775"):');
+
+    const phoneNumber = await question("Ingrese su número de teléfono: ");
+
+    if (!phoneNumber) {
+      errorLog(
+        'Número de teléfono inválido! Intenta nuevamente con el comando "npm start".'
+      );
+
+      process.exit(1);
+    }
+
+    const code = await socket.requestPairingCode(onlyNumbers(phoneNumber));
+
+    sayLog(`Código de emparejamiento: ${code}`);
+  }
+
+  socket.ev.on("connection.update", async (update) => {
+    const { connection, lastDisconnect } = update;
+
+    if (connection === "close") {
+      const statusCode =
+        lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut;
+
+      if (statusCode === DisconnectReason.loggedOut) {
+        errorLog("Krampus está desconectado!");
+      } else {
+        switch (statusCode) {
+          case DisconnectReason.badSession:
+            warningLog("Sección inválida!");
+            break;
+          case DisconnectReason.connectionClosed:
+            warningLog("Conexión cerrada!");
+            break;
+          case DisconnectReason.connectionLost:
+            warningLog("Conexión perdida!");
+            break;
+          case DisconnectReason.connectionReplaced:
+            warningLog("Conexión de reemplazo!");
+            break;
+          case DisconnectReason.multideviceMismatch:
+            warningLog("Dispositivo incompatible!");
+            break;
+          case DisconnectReason.forbidden:
+            warningLog("Conexión prohibida!");
+            break;
+          case DisconnectReason.restartRequired:
+            infoLog('Krampus reiniciado! Inicia con "npm start".');
+            break;
+          case DisconnectReason.unavailableService:
+            warningLog("Servicio no disponible!");
+            break;
+        }
+
+        const newSocket = await connect();
+        load(newSocket);
+      }o
+    } else if (connection === "open") {
+      successLog("Krampus ha sido iniciado!");
+    } else {
+      infoLog("Actualizando conexión...");
+    }
+  });
+
+  socket.ev.on("creds.update", saveCreds);
+
+  return socket;
+}
+
+exports.connect = connect;
