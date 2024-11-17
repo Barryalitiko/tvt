d22e20397a375d91091607f2b7011ef7fcbe073f
Barryalitiko <tiobarryx@gmail.com>
2024-11-17 01:07:04 +0100

    Create off.js

---
+++ src/commands/owner/off.js
@@ -0,0 +1,15 @@
+const { PREFIX } = require("../../config");
+const { deactivateGroup } = require("../../utils/database");
+
+module.exports = {
+  name: "off",
+  description: "Apaga el bot",
+  commands: ["off"],
+  usage: `${PREFIX}off`,
+  handle: async ({ sendSuccessReply, remoteJid }) => {
+    deactivateGroup(remoteJid);
+
+    await sendSuccessReply("👻 `𝙺𝚛𝚊𝚖𝚙𝚞𝚜B𝚘𝚝` 👻
+                           𝚋𝚘𝚝 𝚊𝚙𝚊𝚐𝚊𝚍𝚘");
+  },
+};
