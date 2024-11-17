d934b3cb06dc20ef9def62aed5594a20f466dc62
Barryalitiko <tiobarryx@gmail.com>
2024-11-17 21:03:58 +0100

    Update image.js

--- src/commands/member/image.js
+++ src/commands/member/image.js
@@ -15,7 +15,7 @@ module.exports = {
   }) => {
     if (!fullArgs.length) {
       throw new WarningError(
-        "`👻 𝙺𝚛𝚊𝚖𝚙𝚞𝚜.𝚋𝚘𝚝 👻` 
+        "👻 𝙺𝚛𝚊𝚖𝚙𝚞𝚜.𝚋𝚘𝚝 👻
         añade una descripcion para generar la imagen"
       );
     }
