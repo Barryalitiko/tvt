758e4dd395ca479b05f68cd643c86370bf58387d
Barryalitiko <tiobarryx@gmail.com>
2024-11-17 02:28:25 +0100

    Create sticker.js

---
+++ src/commands/member/sticker.js
@@ -0,0 +1,90 @@
+const { PREFIX, TEMP_DIR } = require("../../config");
+const { InvalidParameterError } = require("../../errors/InvalidParameterError");
+const path = require("path");
+const fs = require("fs");
+const { exec } = require("child_process");
+
+module.exports = {
+  name: "sticker",
+  description: "Convierte stickers",
+  commands: ["s", "sticker"],
+  usage: `${PREFIX}sticker (etiqueta la imagen) ou ${PREFIX}sticker (responda a la imagen)`,
+  handle: async ({
+    isImage,
+    isVideo,
+    downloadImage,
+    downloadVideo,
+    webMessage,
+    sendErrorReply,
+    sendSuccessReact,
+    sendStickerFromFile,
+  }) => {
+    if (!isImage && !isVideo) {
+      throw new InvalidParameterError(
+        " 👻 `𝙺𝚛𝚊𝚖𝚙𝚞𝚜B𝚘𝚝` 👻
+        Marca o responde a una imagen para convertirla a sticker"
+      );
+    }
+
+    const outputPath = path.resolve(TEMP_DIR, "output.webp");
+
+    if (isImage) {
+      const inputPath = await downloadImage(webMessage, "input");
+
+      exec(
+        `ffmpeg -i ${inputPath} -vf scale=512:512 ${outputPath}`,
+        async (error) => {
+          if (error) {
+            console.log(error);
+            fs.unlinkSync(inputPath);
+            throw new Error(error);
+          }
+
+          await sendSuccessReact();
+
+          await sendStickerFromFile(outputPath);
+
+          fs.unlinkSync(inputPath);
+          fs.unlinkSync(outputPath);
+        }
+      );
+    } else {
+      const inputPath = await downloadVideo(webMessage, "input");
+
+      const sizeInSeconds = 10;
+
+      const seconds =
+        webMessage.message?.videoMessage?.seconds ||
+        webMessage.message?.extendedTextMessage?.contextInfo?.quotedMessage
+          ?.videoMessage?.seconds;
+
+      const haveSecondsRule = seconds <= sizeInSeconds;
+
+      if (!haveSecondsRule) {
+        fs.unlinkSync(inputPath);
+
+        await sendErrorReply(`El video que etiquetaste dura mas de ${sizeInSeconds} segundos!`);
+
+        return;
+      }
+
+      exec(
+        `ffmpeg -i ${inputPath} -y -vcodec libwebp -fs 0.99M -filter_complex "[0:v] scale=512:512,fps=12,pad=512:512:-1:-1:color=white@0.0,split[a][b];[a]palettegen=reserve_transparent=on:transparency_color=ffffff[p];[b][p]paletteuse" -f webp ${outputPath}`,
+        async (error) => {
+          if (error) {
+            console.log(error);
+            fs.unlinkSync(inputPath);
+
+            throw new Error(error);
+          }
+
+          await sendSuccessReact();
+          await sendStickerFromFile(outputPath);
+
+          fs.unlinkSync(inputPath);
+          fs.unlinkSync(outputPath);
+        }
+      );
+    }
+  },
+};
