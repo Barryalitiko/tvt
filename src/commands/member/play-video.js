ab7330273e17b99428fe0b89da4d62c478f1403d
Barryalitiko <tiobarryx@gmail.com>
2024-11-17 12:57:08 +0100

    Create play-video.js

---
+++ src/commands/member/play-video.js
@@ -0,0 +1,43 @@
+const { PREFIX } = require("../../config");
+const { playAudio, playVideo } = require("../../services/spider-x-api");
+const { InvalidParameterError } = require("../../errors/InvalidParameterError");
+
+module.exports = {
+  name: "play-video",
+  description: "Descarga videos",
+  commands: ["video", "v"],
+  usage: `${PREFIX}video LOFI Wilmer Roberts`,
+  handle: async ({
+    sendVideoFromURL,
+    args,
+    sendWaitReact,
+    sendSuccessReact,
+    sendErrorReply,
+  }) => {
+    if (!args.length) {
+      throw new InvalidParameterError(
+        "👻 `𝙺𝚛𝚊𝚖𝚙𝚞𝚜B𝚘𝚝` 👻
+        Indicame el video que deseas descargar"
+      );
+    }
+
+    await sendWaitReact();
+
+    try {
+      const data = await playVideo(args[0]);
+
+      if (!data) {
+        await sendErrorReply("👻 `𝙺𝚛𝚊𝚖𝚙𝚞𝚜B𝚘𝚝` 👻
+                             video no encontrado");
+        return;
+      }
+
+      await sendSuccessReact();
+
+      await sendVideoFromURL(data.url);
+    } catch (error) {
+      console.log(error);
+      await sendErrorReply(JSON.stringify(error.message));
+    }
+  },
+};
