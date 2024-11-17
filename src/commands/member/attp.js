9fcd4959641fe075c8d347cbc9bda63f7198d5e4
Barryalitiko <tiobarryx@gmail.com>
2024-11-17 01:52:54 +0100

    Create attp.js

---
+++ src/commands/member/attp.js
@@ -0,0 +1,31 @@
+const { PREFIX } = require("../../config");
+const { attp } = require("../../services/spider-x-api");
+const { InvalidParameterError } = require("../../errors/InvalidParameterError");
+
+module.exports = {
+  name: "attp",
+  description: "Hace figuras animadas de texto.",
+  commands: ["attp"],
+  usage: `${PREFIX}attp teste`,
+  handle: async ({
+    sendWaitReact,
+    args,
+    sendStickerFromURL,
+    sendSuccessReact,
+  }) => {
+    if (!args.length) {
+      throw new InvalidParameterError(
+        "`👻 𝙺𝚛𝚊𝚖𝚙𝚞𝚜.𝚋𝚘𝚝 👻`
+        Debe ingresar el texto que desea convertir en una pegatina"
+      );
+    }
+
+    await sendWaitReact();
+
+    const url = await attp(args[0].trim());
+
+    await sendSuccessReact();
+
+    await sendStickerFromURL(url);
+  },
+};
