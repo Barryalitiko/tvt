5b6a2a3fd05632c3726bb5ea16c215e2ea1a7301
Barryalitiko <tiobarryx@gmail.com>
2024-11-17 20:52:11 +0100

    Update off.js

--- src/commands/owner/off.js
+++ src/commands/owner/off.js
@@ -9,7 +9,7 @@ module.exports = {
   handle: async ({ sendSuccessReply, remoteJid }) => {
     deactivateGroup(remoteJid);
 
-    await sendSuccessReply("👻 `𝙺𝚛𝚊𝚖𝚙𝚞𝚜B𝚘𝚝` 👻
+    await sendSuccessReply("👻 𝙺𝚛𝚊𝚖𝚙𝚞𝚜B𝚘𝚝 👻
                            𝚋𝚘𝚝 𝚊𝚙𝚊𝚐𝚊𝚍𝚘");
   },
 };
